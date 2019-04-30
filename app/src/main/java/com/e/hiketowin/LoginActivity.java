73f8be8115362b5224ae2f7aaf6971b4e1f768a3
Dustin Kowal <dkowal@css.edu>
2019-04-30 15:59:43 -0500

    Users can login but adding options menu to login page makes it crash

--- app/src/main/java/com/e/hiketowin/LoginActivity.java
+++ app/src/main/java/com/e/hiketowin/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.e.hiketowin;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputEditText;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
@@ -22,6 +30,8 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
 
         buttonLogin = (Button) findViewById(R.id.buttonLogin);
         buttonCreate = (Button) findViewById(R.id.buttonCreateAccount);
@@ -42,7 +52,55 @@ public class LoginActivity extends AppCompatActivity {
         String email = textInputEmail.getText().toString();
         String password = textInputPassword.getText().toString();
 
-        //mAuth.createUserWithEmailAndPassword(email, password)
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(getApplicationContext(), "Registration successful!", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                            Toast.makeText(getApplicationContext(), "Registration unsuccessful...", Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_add_hike) {
+            Intent detailHikeIntent = new Intent(getBaseContext(), AddHikeActivity.class);
+            finish();
+            startActivity(detailHikeIntent);
+        }
+        if (id == R.id.action_leaderboard) {
+            Intent mainIntent = new Intent(this, MainActivity.class);
+            finish();
+            startActivity(mainIntent);
+
+        }
+        if (id == R.id.action_duluth_trails) {
+            Intent duluthTrailsIntent = new Intent(getBaseContext(), DuluthTrailsActivity.class);
+            finish();
+            startActivity(duluthTrailsIntent);
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
 }
